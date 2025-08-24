b865be91c9fc08df0d16aed6e20b1f36e5dc180f
haikel fazzani <haikel.fazzani@hotmail.com>
2025-08-24 02:30:16 +0100

    1.0.3

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'stretto',
-      fileName: (format) => format === 'es' ? 'stretto.js' : `stretto.${format}.js`,
+      fileName: (format) => format === 'es' ? 'index.js' : `index.${format}.js`,
       formats: ['es', 'umd'],
     },
     sourcemap: false,
